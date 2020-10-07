d99b85bd771e5ea816fab3d3ce36d6666af63885
hsyliark <sungyoonhwang3@gmail.com>
2020-10-07 20:52:47 +0900

    add

--- JBNU_Statistics_Ph.D/1st semester/고급다변량자료분석/5,6주차/다변량 6주차 과제/6주차 과제.R
+++ JBNU_Statistics_Ph.D/1st semester/고급다변량자료분석/5,6주차/다변량 6주차 과제/6주차 과제.R
@@ -5,7 +5,7 @@ S <- matrix(c(2.879,10.010,-1.810,10.010,199.788,-5.640,-1.810,-5.640,3.628),
 eigen(S)
 qf(0.9,3,17)
 # (b)
-sweet <- read.csv("C:/Users/stat/Desktop/°í±Þ´Ùº¯·®ÀÚ·áºÐ¼®/´Ùº¯·® 6ÁÖÂ÷ °úÁ¦/sweet data_5_4.csv",
+sweet <- read.csv("C:/Users/stat/Desktop/???Þ´Ùº????Ú·??Ð¼?/?Ùº??? 6???? ??ï¿½ï¿½/sweet data_5_4.csv",
                   sep=",", header=T)
 qqnorm(sweet$x1) ; qqline(sweet$x1)
 qqnorm(sweet$x2) ; qqline(sweet$x2)
@@ -16,7 +16,7 @@ plot(sweet)
 # problem 5.20
 # (a)
 library(ggplot2)
-bird <- read.csv("C:/Users/stat/Desktop/°í±Þ´Ùº¯·®ÀÚ·áºÐ¼®/´Ùº¯·® 6ÁÖÂ÷ °úÁ¦/bird data_5_20.csv",
+bird <- read.csv("C:/Users/stat/Desktop/???Þ´Ùº????Ú·??Ð¼?/?Ùº??? 6???? ??ï¿½ï¿½/bird data_5_20.csv",
                  sep=",", header=T)
 ggplot(bird, aes(x1, x2)) +
   geom_point() +
@@ -25,10 +25,10 @@ ggplot(bird, aes(x1, x2)) +
 mean(bird$x1)
 mean(bird$x2)
 var(bird)
-c(193.6222-qnorm(0.975)*sqrt(120.6949/45),193.6222+qnorm(0.975)*sqrt(120.6949/45))
-c(279.7778-qnorm(0.975)*sqrt(208.5404/45),279.7778+qnorm(0.975)*sqrt(208.5404/45))
-c(193.6222-qnorm(0.9875)*sqrt(120.6949/45),193.6222+qnorm(0.9875)*sqrt(120.6949/45))
-c(279.7778-qnorm(0.9875)*sqrt(208.5404/45),279.7778+qnorm(0.9875)*sqrt(208.5404/45))
+c(193.6222-sqrt((2*44/43)*qf(0.95,2,43))*sqrt(120.6949/45),193.6222+sqrt((2*44/43)*qf(0.95,2,43))*sqrt(120.6949/45))
+c(279.7778-sqrt((2*44/43)*qf(0.95,2,43))*sqrt(208.5404/45),279.7778+sqrt((2*44/43)*qf(0.95,2,43))*sqrt(208.5404/45))
+c(193.6222-qt(0.9875,44)*sqrt(120.6949/45),193.6222+qt(0.9875,44)*sqrt(120.6949/45))
+c(279.7778-qt(0.9875,44)*sqrt(208.5404/45),279.7778+qt(0.9875,44)*sqrt(208.5404/45))
 # (c)
 qqnorm(bird$x1) ; qqline(bird$x1)
 qqnorm(bird$x2) ; qqline(bird$x2)
