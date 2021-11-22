b73a579b4d1a1cc4624213023cdedad5531d501b
hsyliark <sungyoonhwang3@gmail.com>
2021-11-22 16:16:06 +0900

    add

--- JBNU_Statistics_Ph.D/Ph.D dissertation/Survival analysis with Machine learning/Censored regression with kernel trick/연구진행/실패 시 진행할 모의실험/ver3/Polynomial vs Gaussian/read csv file.R
+++ JBNU_Statistics_Ph.D/Ph.D dissertation/Survival analysis with Machine learning/Censored regression with kernel trick/연구진행/실패 시 진행할 모의실험/ver3/Polynomial vs Gaussian/read csv file.R
@@ -1,5 +1,5 @@
-dat.res5 <- read.csv("C:/Users/Hi/Desktop/ver3/Polynomial vs Gaussian/설명변수 5/중도절단 0/p5cen0.csv",sep=",",header=T)
-dat.res5 <- dat.res4[,-1]
-dat.res5_1 <-dat.res5[dat.res5$number=="50",]
-dat.res5_2 <-dat.res5[dat.res5$number=="100",]
-dat.res5_3 <-dat.res5[dat.res5$number=="200",]
+dat.res16 <- read.csv("C:/Users/Hi/Desktop/ver3/Polynomial vs Gaussian/설명변수 9/중도절단 50/p9cen50.csv",sep=",",header=T)
+dat.res16 <- dat.res16[,-1]
+dat.res16_1 <-dat.res16[dat.res16$number=="50",]
+dat.res16_2 <-dat.res16[dat.res16$number=="100",]
+dat.res16_3 <-dat.res16[dat.res16$number=="200",]
