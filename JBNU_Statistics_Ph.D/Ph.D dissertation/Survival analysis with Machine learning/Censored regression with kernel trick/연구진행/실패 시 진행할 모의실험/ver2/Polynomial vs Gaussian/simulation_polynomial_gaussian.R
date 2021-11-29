90983ff235394d575ebb709fce4edd897ba7f670
hsyliark <sungyoonhwang3@gmail.com>
2021-11-30 00:01:36 +0900

    add

--- JBNU_Statistics_Ph.D/Ph.D dissertation/Survival analysis with Machine learning/Censored regression with kernel trick/연구진행/실패 시 진행할 모의실험/ver2/Polynomial vs Gaussian/simulation_polynomial_gaussian.R
+++ JBNU_Statistics_Ph.D/Ph.D dissertation/Survival analysis with Machine learning/Censored regression with kernel trick/연구진행/실패 시 진행할 모의실험/ver2/Polynomial vs Gaussian/simulation_polynomial_gaussian.R
@@ -2058,16 +2058,16 @@ fit.ftn <- function(number1, number2, a) {
 library(ggplot2)
 
 # Boxplot for one dataset
-ggplot(dat.res8_1, aes(x = method, y = RMSE, fill = method)) + geom_boxplot() 
-ggplot(dat.res8_2, aes(x = method, y = RMSE, fill = method)) + geom_boxplot()
-ggplot(dat.res8_3, aes(x = method, y = RMSE, fill = method)) + geom_boxplot()
+ggplot(dat.res12_1, aes(x = method, y = RMSE, fill = method)) + geom_boxplot() 
+ggplot(dat.res12_2, aes(x = method, y = RMSE, fill = method)) + geom_boxplot()
+ggplot(dat.res12_3, aes(x = method, y = RMSE, fill = method)) + geom_boxplot()
 # Boxplot for row binded dataset
-dat.res8[dat.res8$number==50,]$number <- "1(50)"
-dat.res8[dat.res8$number==100,]$number <- "2(100)"
-dat.res8[dat.res8$number==200,]$number <- "3(200)"
-ggplot(dat.res8, aes(x = number, y = RMSE, fill = number)) + geom_boxplot() +
+dat.res12[dat.res12$number==50,]$number <- "1(50)"
+dat.res12[dat.res12$number==100,]$number <- "2(100)"
+dat.res12[dat.res12$number==200,]$number <- "3(200)"
+ggplot(dat.res12, aes(x = number, y = RMSE, fill = number)) + geom_boxplot() +
   facet_wrap(~ method, ncol=16) + theme(axis.text.x=element_text(angle=45, hjust=1))
-write.csv(dat.res8, "C:/Users/Hi/Desktop/ver2/Polynomial vs Gaussian/p5cen50.csv")
+write.csv(dat.res12, "C:/Users/Hi/Desktop/ver2/Polynomial vs Gaussian/p7cen50.csv")
 
 
 # Print result
