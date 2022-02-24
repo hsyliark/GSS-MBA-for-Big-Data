b6fc5959d010b70916c3ecdfc189fa7108abd743
hsyliark <sungyoonhwang3@gmail.com>
2022-02-24 21:17:19 +0900

    add

--- JBNU_Statistics_Ph.D/Ph.D dissertation/Survival analysis with Machine learning/주제2/calibration (post processing)/발표준비(220221~220225)/모의실험/AUC 비교(GB vs ER)/code1.R
+++ JBNU_Statistics_Ph.D/Ph.D dissertation/Survival analysis with Machine learning/주제2/calibration (post processing)/발표준비(220221~220225)/모의실험/AUC 비교(GB vs ER)/code1.R
@@ -12,3 +12,18 @@ Sigma.hat <- solve(I.inv[c(6,7,8),c(6,7,8)])
 library(survMisc)
 
 gof(cox0, G=11) # GB test
+
+
+# Kaplan-Meier estimator
+library(survival)
+uis2 <- read.csv("C:/Users/HSY/Desktop/uis2.csv", header=T, sep=",")
+km <- with(uis2, Surv(time, censor))
+km
+km_fit <- survfit(Surv(time, censor) ~ 1, data=uis2)
+summary(km_fit)
+km_fit[["cumhaz"]] # cumulative hazard function
+km_fit[["surv"]] # survival function
+summary(km_fit, times = c(1,5*(1:20)))
+
+# time dependent ROC curve
+library(timeROC)
