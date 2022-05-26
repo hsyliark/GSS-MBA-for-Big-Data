b9853b250c75809f61767e0b1b3df90488be0c96
hsyliark <sungyoonhwang3@gmail.com>
2022-05-26 15:39:16 +0900

    add

---
+++ JBNU_Statistics_Ph.D/Ph.D dissertation/Survival analysis with Machine learning/주제2/calibration (post processing)/회의준비(220530)/Concordance index.R
@@ -0,0 +1,79 @@
+# reference : https://www.erikdrysdale.com/survConcordance/
+
+## Function for the concordance index (C-Index)
+
+# Function for the i'th concordance
+cindex_i <- function(So,eta,i) {
+  tt_i <- So[i,1]
+  dd_i <- So[i,2]
+  idx.k <- which(So[,1] > tt_i)
+  conc <- sum(eta[i] > eta[idx.k]  )
+  disc <- sum(eta[i] < eta[idx.k]  )
+  return(c(conc,disc))
+}
+
+# Wrapper for total concordance
+cindex <- function(So,eta) {
+  conc.disc <- c(0,0)
+  for (i in which(So[,2] == 1)) {
+    conc.disc <- conc.disc + cindex_i(So,eta,i)
+  }
+  names(conc.disc) <- c('concordant','discordant')
+  return(conc.disc)
+}
+
+# Example of the 'veteran' data
+library(survival)
+So <- with(veteran,Surv(time,status==1))
+X <- model.matrix(~factor(trt)+karno+diagtime+age+factor(prior),data=veteran)[,-1]
+eta <- predict(coxph(So ~ X))
+Som <- as.matrix(So)
+Som[,1] <- Som[,1] + (1-Som[,2])*min(Som[,1])/2
+survConcordance.fit(So, eta)[1:2]
+
+
+## Equation (2)
+
+sigmoid <- function(x) { 1/(1+exp(-x)) }
+
+l_eta_i <- function(So,eta,i) {
+  tt_i <- So[i,1]
+  dd_i <- So[i,2]
+  idx.k.i <- which(So[,1] > tt_i)
+  loss.i <- sum(1 + log( sigmoid(eta[i] - eta[idx.k.i]) )/log(2) )
+  return(loss.i)
+}
+
+l_eta <- function(So, eta) {
+  loss <- 0
+  for (i in which(So[,2] == 1)) {
+    loss <- loss + l_eta_i(So,eta,i)
+  }
+  return(-loss / nrow(So)^2)
+}
+
+
+## Equation (3)
+
+sigmoid2 <- function(x) { 1/(1+exp(x)) }
+
+dl_eta_i <- function(eta,So,i) {
+  tt_i <- So[i,1]
+  dd_i <- So[i,2]
+  idx.k <- which(So[,1] > tt_i)
+  idx.j <- which(tt_i > So[,1] & So[,2]==1) 
+  res.i <- dd_i*sum(sigmoid2(eta[i] - eta[idx.k])) - sum(sigmoid2(eta[idx.j] - eta[i]))
+  return(res.i)
+}
+
+dl_eta <- function(X,eta,So) {
+  grad <- rep(0, ncol(X))
+  for (i in seq_along(eta)) {
+    grad <- grad + X[i,] * dl_eta_i(eta, So, i)
+  }
+  grad <- -1 * grad / nrow(X)^2
+  return(grad)
+}
+
+
+
